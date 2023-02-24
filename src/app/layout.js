ade2705b5cee8949909f97eb6efe337622a5b637
marnvel <ricardom.velarde@gmail.com>
2023-02-24 17:32:29 -0300

    Creation of the header seccition 24-02-2023 17:32hs

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import Header from '@/components/Header'
 import './globals.css'
 
 export default function RootLayout({ children }) {
@@ -8,7 +9,22 @@ export default function RootLayout({ children }) {
         head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       {/* <head /> */}
-      <body>{children}</body>
+      <body>
+        {/* Header */}
+
+        <Header/>
+
+
+        {/* Navbar */}
+
+
+
+        {/* SearchBox */}
+
+
+
+        {children}
+      </body>
     </html>
   )
 }
