06032ec8fca00f2b9a683a9967f92174b388ba21
marnvel <ricardom.velarde@gmail.com>
2023-02-27 13:28:29 -0300

    Fetch data from tmdb api in the home page. 27-02-2023 13:29hs

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,39 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Results from "@/components/Results";
 
+const API_KEY = process.env.API_KEY1;
+// console.log(API_KEY)
 
-const inter = Inter({ subsets: ['latin'] })
+/**
+ * Para hacer el fetching no usamos ningún hook porque esto sería usar el "client side".
+ * Nosotros vamos a hacer el fetchin desde el "server side"
+ */
 
-export default function Home() {
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
+
+  const res = await fetch(
+    `
+    https://api.themoviedb.org/3/${
+        genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+      }?api_key=${API_KEY}&language=es&page=1
+    `,
+    { next: { revalidate: 18000 } }
+  );
+
+  if(!res.ok){
+    throw new Error('Failed to fetch data'); // Esto será capturado por la página de error y pasado a la página como "prop"
+  }
+
+  const data = await res.json();
+  const results = data.results;
+  /* 
+  console.log(results); 
+  !Como estamos haciendo el fetch del lado del servidor, sólo seremos capaces de ver los resultados en la terminal de nuestro IDE y no en el navegador. 
+  */
+
   return (
-    <h1 className='text-red-400'>HOME bro</h1>
-  )
+    <div>
+      <Results results={results} />
+    </div>
+    );
 }
