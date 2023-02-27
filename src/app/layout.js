70f670e924b84bf252c374a782d617a20f0a75e1
marnvel <ricardom.velarde@gmail.com>
2023-02-27 09:38:42 -0300

    navbar cration and styling. 27-02-2023 09:38hs

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import Header from '@/components/Header'
 import { Providers } from './Providers'
 import './globals.css'
+import Navbar from '@/components/Navbar'
 
 export default function RootLayout({ children }) {
   return (
@@ -17,6 +18,7 @@ export default function RootLayout({ children }) {
           <Header/>
 
           {/* Navbar */}
+          <Navbar />
 
           {/* SearchBox */}
 
