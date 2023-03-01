47e98547022cce4ff0457ea6471448aeece5223e
marnvel <ricardom.velarde@gmail.com>
2023-03-01 08:46:57 -0300

    Se arreglaron importaciones para hacer andar el modo desarrollo. 01-03-2023 08:46hs

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,7 @@ import Header from '@/components/Header'
 import Navbar from '@/components/Navbar'
 import SearchBox from '@/components/SearchBox'
 
-import Providers from './Providers'
+import { Providers } from './Providers'
 
 import './globals.css'
 
