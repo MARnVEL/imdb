e27eec401ac8e31d853675c84ff55e4e992db6d8
marnvel <ricardom.velarde@gmail.com>
2023-02-28 23:08:16 -0300

    cambios despliegue

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,8 @@
-export const dynamic = "force-dynamic"; // this is the fix
+import Results from "@/components/Results";
+
+// export const dynamic = "force-dynamic"; // this is the fix
 // Fix the problem for "search params object is empty in production with next 13 app dir"
 // for more info "https://github.com/vercel/next.js/issues/43077"
-
-import Results from "@/components/Results";
 
 const API_KEY = process.env.API_KEY1;
 // console.log(API_KEY)
