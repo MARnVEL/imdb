94a72d172d14e50da474a73143b5624655624de1
marnvel <ricardom.velarde@gmail.com>
2023-03-01 09:04:08 -0300

    Cambios menores

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import Results from "@/components/Results";
 
-// export const dynamic = "force-dynamic"; // this is the fix
+export const dynamic = "force-dynamic"; // this is the fix
 // Fix the problem for "search params object is empty in production with next 13 app dir"
 // for more info "https://github.com/vercel/next.js/issues/43077"
 
